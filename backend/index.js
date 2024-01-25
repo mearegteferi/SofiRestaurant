32afeb68e7b9f8e83c1f6932eb43abecc7649191
mearegteferi <157679708+mearegteferi@users.noreply.github.com>
2024-01-24 23:29:51 -0800

    add api payment

---
+++ backend/index.js
@@ -0,0 +1,243 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const Stripe = require("stripe");
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+
+const PORT = process.env.PORT || 8080;
+
+//mongodb connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connect to Databse"))
+  .catch((err) => console.log(err));
+
+//schema
+const userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  confirmPassword: String,
+  image: String,
+});
+
+//
+const userModel = mongoose.model("user", userSchema);
+
+//api
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+//sign up
+app.post("/signup", async (req, res) => {
+  // console.log(req.body);
+  const { email } = req.body;
+
+  userModel.findOne({ email: email }, (err, result) => {
+    // console.log(result);
+    console.log(err);
+    if (result) {
+      res.send({ message: "Email id is already registered", alert: false });
+    } else {
+      const data = userModel(req.body);
+      const save = data.save();
+      res.send({ message: "Successfully sign up", alert: true });
+    }
+  });
+});
+
+//api login
+app.post("/login", (req, res) => {
+  // console.log(req.body);
+  const { email } = req.body;
+  userModel.findOne({ email: email }, (err, result) => {
+    if (result) {
+      const dataSend = {
+        _id: result._id,
+        firstName: result.firstName,
+        lastName: result.lastName,
+        email: result.email,
+        image: result.image,
+      };
+      console.log(dataSend);
+      res.send({
+        message: "Login is successfully",
+        alert: true,
+        data: dataSend,
+      });
+    } else {
+      res.send({
+        message: "Email is not available, please sign up",
+        alert: false,
+      });
+    }
+  });
+});
+
+//product section
+
+const schemaProduct = mongoose.Schema({
+  name: String,
+  category: String,
+  image: String,
+  price: String,
+  description: String,
+});
+const productModel = mongoose.model("product", schemaProduct);
+
+const orderItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  quantity: {
+    type: Number,
+  },
+});
+
+// Schema for customer information
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+});
+
+// Main order schema
+const orderSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  phone: {
+    type: String  ,
+  },
+  items: {
+    type: [orderItemSchema],
+  },
+  totalPrice: {
+    type: Number,
+  },
+  totalQuantity: {
+    type: Number,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Processing", "Delivered"], // Add other possible statuses as needed
+    default: "Pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+//save product in data
+//api
+app.post("/uploadProduct", async (req, res) => {
+  // console.log(req.body)
+  const data = await productModel(req.body);
+  const datasave = await data.save();
+  res.send({ message: "Upload successfully" });
+});
+
+app.post("/orderFood", async (req, res) => {
+  const { data, productCartItem, totalP, totalQ } = req.body;
+  const { name, email, address, phone } = data;
+
+  const items = productCartItem.map(item => ({
+    name: item.name,
+    price: item.price,
+    quantity: item.quantity,
+  }));
+
+  const order = new Order({
+    name,
+    email,
+    address,
+    phone,
+    items,
+    totalPrice: totalP,
+    totalQuantity: totalQ,
+  });
+  
+  await order.save();
+  res.send({ message: "ordered successfully" });
+});
+
+//
+app.get("/product", async (req, res) => {
+  const data = await productModel.find({});
+  res.send(JSON.stringify(data));
+});
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const params = {
+      submit_type: "pay",
+      mode: "payment",
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      shipping_options: [{ shipping_rate: "shr_1OaGkFHKXtDioQ8QEsJQ7Gef" }],
+
+      line_items: req.body.map((item) => {
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: item.name,
+              // images : [item.image]
+            },
+            unit_amount: item.price * 100,
+          },
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+          },
+          quantity: item.qty,
+        };
+      }),
+
+      success_url: `${process.env.FRONTEND_URL}/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+    };
+
+    const session = await stripe.checkout.sessions.create(params);
+    // console.log(session)
+    res.status(200).json(session.id);
+  } catch (err) {
+    res.status(err.statusCode || 500).json(err.message);
+  }
+});
+
+//server is ruuning
+app.listen(PORT, () => console.log("server is running at port : " + PORT));
